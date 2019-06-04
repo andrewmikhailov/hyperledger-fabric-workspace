b9499727d678ea576dc01b6be3cc025d0494f3e3
Vitche Research Team <developer@vitche.com>
2019-06-04 16:29:16 +0000

    1. Final version of the sample chaincode deployment script.

--- chaincodes/sample/sample.sh
+++ chaincodes/sample/sample.sh
@@ -1,2 +1,2 @@
-peer chaincode install -p github.com/chaincodes/sample/sample -n sample -v 0
+peer chaincode install -p github.com/chaincodes/sample -n sample -v 0
 peer chaincode instantiate -n sample -v 0 -c '{"Args":[]}' -C mychannel
