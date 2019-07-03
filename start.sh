b2fe6a93f3aecd3253d6eaec079b416f610473b8
Vitche Research Team <developer@vitche.com>
2019-07-03 17:21:05 +0000

    1. Starting the Docker host shell tunnel in the main network file.

--- start.sh
+++ start.sh
@@ -4,8 +4,26 @@ set -ev
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 
+startHostShellTunnel() {
+
+	echo "Starting Docker host shell tunnel"
+
+	USER=ubuntu
+	INPUT_FIFO=/tmp/docker-host-shell-tunnel-input.fifo 
+	OUTPUT_FIFO=/tmp/docker-host-shell-tunnel-output.fifo
+
+	# Clean-up previous FIFO worker data
+	rm $INPUT_FIFO $OUTPUT_FIFO
+
+	# Start the FIFO worker to process incoming command requests
+	touch $INPUT_FIFO $OUTPUT_FIFO
+	chown $USER $INPUT_FIFO $OUTPUT_FIFO
+	./worker.sh $INPUT_FIFO $OUTPUT_FIFO &
+}
+
+startHostShellTunnel
 docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb cli
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb cli host-shell0.org1.example.com
 docker ps -a
 
 # wait for Hyperledger Fabric to start
