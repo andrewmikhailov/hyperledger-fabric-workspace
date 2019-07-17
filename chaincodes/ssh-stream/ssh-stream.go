c53fdd857ed29d2b5e7bdff54dc2acf9cb8ec3dc
Vitche Research Team <developer@vitche.com>
2019-07-17 17:17:57 +0000

    1. Chain-code development mode for the network. 2. Debugging log level. 3. Integrated SHELL tunnel to the chain-code.

--- chaincodes/ssh-stream/ssh-stream.go
+++ chaincodes/ssh-stream/ssh-stream.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"time"
+	"bufio"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/hyperledger/fabric/core/chaincode/lib/cid"
+	// "github.com/hyperledger/fabric/core/chaincode/lib/cid"
 	"github.com/hyperledger/fabric/protos/peer"
 )
 
@@ -16,13 +19,48 @@ func (t *SSHStream) Init(stub shim.ChaincodeStubInterface) peer.Response {
 
 func (t *SSHStream) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 
-	id, _ := cid.GetID(stub)
+	// id, _ := cid.GetID(stub)
 
-	// function, _ := stub.GetFunctionAndParameters()
-	return shim.Success([]byte(id))
+	function, arguments := stub.GetFunctionAndParameters()
+	if "eval" == function {
+
+		tunnelUri := arguments[0]
+		command := arguments[1]
+
+		fmt.Printf("SHELL command:\n%s\n", command)
+		fmt.Printf("On tunnel:\n%s\n", tunnelUri)
+
+		// Execute the desired SHELL command
+		connection, error := net.Dial("tcp", tunnelUri)
+		fmt.Fprintf(connection, command)
+		time.Sleep(1 * time.Second)
+		connection.Close()
+		time.Sleep(1 * time.Second)
+
+		// Read the SHELL response
+		content := ""
+		connection, error = net.Dial("tcp", tunnelUri)
+		if nil == error {
+			reader := bufio.NewReader(connection)
+			atom := ""
+			error = nil
+			for nil == error {
+				atom, error = reader.ReadString('\n')
+				content += atom
+			}
+		}
+
+		fmt.Printf("SHELL response:\n%s", content)
+
+		response := string(content)
+		return shim.Success([]byte(response))
+	}
+
+	return shim.Success([]byte("ERROR_UNKNOWN"))
 }
 
 func main() {
+
 	if error := shim.Start(new(SSHStream)); error != nil {
 		fmt.Printf("Error starting 'SSHStream' chaincode: %s", error)
 	}
--- chaincodes/ssh-stream/ssh-stream.sh
+++ chaincodes/ssh-stream/ssh-stream.sh
@@ -9,9 +9,9 @@ case $1 in
 		peer chaincode upgrade -p github.com/chaincodes/ssh-stream -n ssh-stream -v $VERSION -c '{"Args":[]}' -C mychannel
 		;;
 	invoke)
-		peer chaincode invoke -n ssh-stream -c '{"Args":["ping"]}' -C mychannel
+		peer chaincode invoke -n ssh-stream -c '{"Args":["eval", "ls"]}' -C mychannel
 		;;
 	test)
-		CORE_PEER_ADDRESS=localhost:7052 CORE_CHAINCODE_ID_NAME=ssh-stream:$VERSION ./ssh-stream
+		CORE_PEER_ADDRESS=localhost:7052 CORE_CHAINCODE_ID_NAME=ssh-stream:$VERSION CORE_CHAINCODE_LOGGING_LEVEL=debug ./ssh-stream
 		;;
 esac
