660801a1bd7dd9dd46e1610c318a6f9d6a75c271
Vitche Research Team <developer@vitche.com>
2019-10-10 16:00:37 +0000

    1. A script to deploy a chain-code with a random name.

--- chaincodes/standalone/standalone.sh
+++ chaincodes/standalone/standalone.sh
@@ -1,8 +1,10 @@
 VERSION=1
+NODE_NAME="$(od -x /dev/urandom | head -1 | awk '{OFS=""; print $2$3,$4,$5,$6,$7$8$9}')"
 case $1 in
 	install)
-		peer chaincode install -p github.com/chaincodes/standalone -n standalone -v $VERSION
-		peer chaincode instantiate -n standalone -v $VERSION -c '{"Args":[]}' -C mychannel
+		echo $NODE_NAME
+		peer chaincode install -p github.com/chaincodes/standalone -n $NODE_NAME -v $VERSION
+		# peer chaincode instantiate -n standalone -v $VERSION -c '{"Args":[]}' -C mychannel
 		;;
 	upgrade)
 		peer chaincode install -p github.com/chaincodes/standalone -n standalone -v $VERSION
