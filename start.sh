33a04c9aaefdb0b08507024579256a8dd40d664d
Vitche Research Team <developer@vitche.com>
2019-10-10 15:24:20 +0000

    1. Minor changes to the main workspace to make it more stable.

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#
-# Copyright IBM Corp All Rights Reserved
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+
 # Exit on first error, print all commands.
 set -ev
 
@@ -17,7 +13,7 @@ docker ps -a
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=10
+export FABRIC_START_TIMEOUT=20
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
