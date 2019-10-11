9282c48a292537f12352c092d0723ef2e131b876
Vitche Research Team <developer@vitche.com>
2019-10-11 14:17:16 +0000

    1. Updated the script to call uniquely identified chain-codes.

--- chaincodes/standalone/standalone.sh
+++ chaincodes/standalone/standalone.sh
@@ -1,16 +1,17 @@
 VERSION=1
-NODE_NAME="$(od -x /dev/urandom | head -1 | awk '{OFS=""; print $2$3,$4,$5,$6,$7$8$9}')"
 case $1 in
 	install)
-		echo $NODE_NAME
+		NODE_NAME="$(od -x /dev/urandom | head -1 | awk '{OFS=""; print $2$3,$4,$5,$6,$7$8$9}')"
 		peer chaincode install -p github.com/chaincodes/standalone -n $NODE_NAME -v $VERSION
-		# peer chaincode instantiate -n standalone -v $VERSION -c '{"Args":[]}' -C mychannel
+		# TODO: In the multiple OS environment, need to separate instantiation and installation
+		peer chaincode instantiate -n $NODE_NAME -v $VERSION -c '{"Args":[]}' -C mychannel
+		echo "{name: \"$NODE_NAME\"}"
 		;;
 	upgrade)
-		peer chaincode install -p github.com/chaincodes/standalone -n standalone -v $VERSION
-		peer chaincode upgrade -p github.com/chaincodes/standalone -n standalone -v $VERSION -c '{"Args":[]}' -C mychannel
+		peer chaincode install -p github.com/chaincodes/standalone -n $2 -v $VERSION
+		peer chaincode upgrade -p github.com/chaincodes/standalone -n $2 -v $VERSION -c '{"Args":[]}' -C mychannel
 		;;
 	invoke)
-		peer chaincode invoke -n standalone -c '{"Args":["ping"]}' -C mychannel
+		peer chaincode invoke -n $2 -c '{"Args":["ping"]}' -C mychannel
 		;;
 esac
