64471a89b47113bcca37a72cace86a9966f8cbfd
Vitche Research Team <developer@vitche.com>
2019-11-11 16:21:14 +0000

    1. TOR mapping.

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb cli
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb cli peer0.org1.example.onion
 docker ps -a
 
 # wait for Hyperledger Fabric to start
