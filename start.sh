1eb836616767915b4360faeeba357637868daf01
Vitche Research Team Developer <developer@vitche.com>
2019-12-23 18:55:22 -0500

    1. Generating administrator certificate on CA and using that certificate.

--- start.sh
+++ start.sh
@@ -16,9 +16,11 @@ docker ps -a
 export FABRIC_START_TIMEOUT=20
 sleep ${FABRIC_START_TIMEOUT}
 
+./ca.sh
+
 # Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c channel -f /etc/hyperledger/configtx/channel.tx
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Administrator@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c channel -f /etc/hyperledger/configtx/channel.tx
 
 # Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b channel.block
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Administrator@org1.example.com/msp" peer0.org1.example.com peer channel join -b channel.block
 
