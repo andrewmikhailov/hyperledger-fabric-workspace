c87943109ec308d8662ac06299ab6c824590f444
Vitche Research Team Developer <developer@vitche.com>
2020-01-05 17:04:27 -0500

    1. Updated the application channel name.

--- chaincodes/shell/chaincode.sh
+++ chaincodes/shell/chaincode.sh
@@ -4,17 +4,17 @@ case $1 in
 		NODE_NAME="$(od -x /dev/urandom | head -1 | awk '{OFS=""; print $2$3,$4,$5,$6,$7$8$9}')"
 		peer chaincode install -p github.com/chaincodes/shell -n $NODE_NAME -v $VERSION
 		# TODO: In the multiple OS environment, need to separate instantiation and installation
-		peer chaincode instantiate -n $NODE_NAME -v $VERSION -c '{"Args":[]}' -C mychannel
+		peer chaincode instantiate -n $NODE_NAME -v $VERSION -c '{"Args":[]}' -C channel
 		echo "{name: \"$NODE_NAME\"}"
 		;;
 	upgrade)
 		peer chaincode install -p github.com/chaincodes/shell -n $2 -v $VERSION
-		peer chaincode upgrade -p github.com/chaincodes/shell -n $2 -v $VERSION -c '{"Args":[]}' -C mychannel
+		peer chaincode upgrade -p github.com/chaincodes/shell -n $2 -v $VERSION -c '{"Args":[]}' -C channel
 		;;
 	invoke)
-		peer chaincode invoke -n $2 -c '{"Args":["eval", "ls"]}' -C mychannel
+		peer chaincode invoke -n $2 -c '{"Args":["eval", "ls"]}' -C channel
 		;;
 	shell)
-		peer chaincode invoke -n $2 -c '{"Args":["eval", "'$3'"]}' -C mychannel
+		peer chaincode invoke -n $2 -c '{"Args":["eval", "'$3'"]}' -C channel
 		;;
 esac
