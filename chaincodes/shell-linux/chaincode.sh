52274ae601389aabe2615293192494e00cb71641
Vitche Research Team <developer@vitche.com>
2020-04-22 18:00:38 -0400

    1. Renamed the "shell" chain-code into "shell-linux".

--- chaincodes/shell-linux/chaincode.sh
+++ chaincodes/shell-linux/chaincode.sh
@@ -2,24 +2,24 @@ VERSION=1
 case $1 in
 	install)
 		NODE_NAME="$(od -x /dev/urandom | head -1 | awk '{OFS=""; print $2$3,$4,$5,$6,$7$8$9}')"
-		peer chaincode install -p github.com/chaincodes/shell -n $NODE_NAME -v $VERSION
+		peer chaincode install -p github.com/chaincodes/shell-linux -n $NODE_NAME -v $VERSION
 		# TODO: In the multiple OS environment, need to separate instantiation and installation
 		peer chaincode instantiate -n $NODE_NAME -v $VERSION -c '{"Args":[]}' -C channel
 		echo "{name: \"$NODE_NAME\"}"
 		;;
 	upgrade)
-		peer chaincode install -p github.com/chaincodes/shell -n $2 -v $VERSION
-		peer chaincode upgrade -p github.com/chaincodes/shell -n $2 -v $VERSION -c '{"Args":[]}' -C channel
+		peer chaincode install -p github.com/chaincodes/shell-linux -n $2 -v $VERSION
+		peer chaincode upgrade -p github.com/chaincodes/shell-linux -n $2 -v $VERSION -c '{"Args":[]}' -C channel
 		;;
 	invoke)
 		peer chaincode invoke -n $2 -c '{"Args":["eval", "ls"]}' -C channel
 		;;
-	shell)
+	shell-linux)
 		peer chaincode invoke -n $2 -c '{"Args":["eval", "'$3'"]}' -C channel
 		;;
 	test)
 		CORE_PEER_ADDRESS=localhost:7052
 		# CORE_PEER_ADDRESS=peer0.org1.example.com:7052
-		CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS CORE_CHAINCODE_ID_NAME=$2:1 CORE_CHAINCODE_LOGGING_LEVEL=debug ./shell
+		CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS CORE_CHAINCODE_ID_NAME=$2:1 CORE_CHAINCODE_LOGGING_LEVEL=debug ./shell-linux
 		;;
 esac
