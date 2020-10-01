501d0ccaf156ad5a1770aef677d7401dbfdf7a1d
Vitche Research Team <developer@vitche.com>
2020-09-30 21:58:52 -0400

    1. Automatically installing agent as an "init.d" daemon.

--- chaincodes/shell-raspberry/start.tpl.sh
+++ chaincodes/shell-raspberry/start.tpl.sh
@@ -9,20 +9,34 @@
 ### END INIT INFO
 
 # Settings
-BINARY_NAME=./shell-raspberry
+FOLDER_NAME=$(dirname $(readlink -f "$0"))
+BINARY_NAME="$FOLDER_NAME/shell-raspberry"
 CHAINCODE_ID_NAME={CHAINCODE_ID_NAME}
 CHAINCODE_VERSION={CHAINCODE_VERSION}
 PEER_ADDRESS={PEER_ADDRESS}
+export TOKENIZER_ENDPOINT=http://softethica.com:3007/keyChain/password
 
 # Methods
 start() {
   CORE_PEER_ADDRESS=$PEER_ADDRESS CORE_CHAINCODE_ID_NAME=$CHAINCODE_ID_NAME:$CHAINCODE_VERSION CORE_CHAINCODE_LOGGING_LEVEL=debug $BINARY_NAME
+}
+install() {
+  link=/etc/init.d/hyshd
+  if [ -f $link ] ; then
+    rm $link
+  fi
+  ln -s "$FOLDER_NAME/start.sh" $link
+  update-rc.d hyshd defaults
 }
 
 case "$1" in
   start)
     start
     ;;
+  install)
+    install
+    ;;
   *)
-    echo "Usage: $0 {start}"
+    echo "Usage: $0 {start|install}"
+    ;;
 esac
