86aad9d7f9f0c1d763e2d0ff122c8be276d0c957
Vitche Research Team <developer@vitche.com>
2021-08-25 16:43:05 +0300

    1. Removed the port replicator for simplicity.

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb installer gateway scheduler cli port-replicator-peer-1 port-replicator-peer-2 port-replicator-peer-3 port-replicator-couchdb port-replicator-installer port-replicator-gateway port-replicator-scheduler
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb installer gateway scheduler cli
 docker ps -a
