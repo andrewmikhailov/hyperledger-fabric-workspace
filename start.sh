6d5a2db6cbd798e5294d8b7ffdcac93707a60d0d
Vitche Research Team <developer@vitche.com>
2021-09-11 12:59:09 +0300

    1. A working version of the streaming nginx proxy.

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com peer1.org1.example.com couchdb installer gateway scheduler cli ide
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com peer1.org1.example.com couchdb installer gateway scheduler cli ide tls
 docker ps -a
